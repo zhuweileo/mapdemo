ed17bee7e57656b6e364e4f6498c3c43a4cb7734
zhuwei <zhuwei_leo@163.com>
2020-03-28 15:29:51 +0800

    feat: 部署

---
+++ build/precache-manifest.a806e2d9f0620d6b7ec9a3bff2553f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54749daa781e86818cd316eb05365cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd101cdcbc187b980941",
+    "url": "/static/css/main.a785caaf.chunk.css"
+  },
+  {
+    "revision": "65d5afb64f2a1f95d52d",
+    "url": "/static/js/2.ed48ef3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed48ef3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd101cdcbc187b980941",
+    "url": "/static/js/main.27bc02b3.chunk.js"
+  },
+  {
+    "revision": "4dc5e6d36e1df17ff603",
+    "url": "/static/js/runtime-main.4cdf6972.js"
+  }
+]);
