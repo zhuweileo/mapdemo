0a41435ba24fbf2215d0ce25f4bac779873a7bbf
zhuwei <zhuwei_leo@163.com>
2020-03-28 15:42:23 +0800

    feat: 修改部署路径

---
+++ build/precache-manifest.0ac68d7c590cfb2e6b270aa0f2092f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5fb951b0344a1885181d1f8d41bdd4b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9d6b8ebcef989684a82e",
+    "url": "./static/css/main.a785caaf.chunk.css"
+  },
+  {
+    "revision": "65d5afb64f2a1f95d52d",
+    "url": "./static/js/2.ed48ef3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ed48ef3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d6b8ebcef989684a82e",
+    "url": "./static/js/main.38dd0891.chunk.js"
+  },
+  {
+    "revision": "e6ee687fb975d51a3afe",
+    "url": "./static/js/runtime-main.ae7b7a09.js"
+  }
+]);
